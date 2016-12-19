0fa43771078b3dc89cf99d5247334ceff3915cef
Administrator <1247611843@qq.com>
2016-12-19 14:31:31 +0800

    photoview相应代码

--- app/src/main/java/com/jiujiu/ecdemo/photoview/log/LoggerDefault.java
+++ app/src/main/java/com/jiujiu/ecdemo/photoview/log/LoggerDefault.java
@@ -33,6 +33,30 @@ public class LoggerDefault implements Logger {
         return Log.i(tag, msg);
     }
 
+    @Override
+    public int i(String tag, String msg, Throwable tr) {
+        return Log.i(tag, msg, tr);
+    }
+
+    @Override
+    public int w(String tag, String msg) {
+        return Log.w(tag, msg);
+    }
+
+    @Override
+    public int w(String tag, String msg, Throwable tr) {
+        return Log.w(tag, msg, tr);
+    }
+
+    @Override
+    public int e(String tag, String msg) {
+        return Log.e(tag, msg);
+    }
+
+    @Override
+    public int e(String tag, String msg, Throwable tr) {
+        return Log.e(tag, msg, tr);
+    }
 
 
 }
